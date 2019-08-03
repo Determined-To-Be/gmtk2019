a28be60a8836072980256c7add8e81605f774c0b
Jacob Golden-Needham <jacob@goldenneedham.com>
2019-08-02 22:02:23 -0700

    Working on tileTIme

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+	public float maxSpeed = 1f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void move(Vector2 direction) {
+		//We need to prevent the character from moving in the direction of a wall or a collision
+		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction);
+		if (hit){ //I hit something I can't move here
+
+		}
+	}
+}
---
+++ Assets/Scripts/TileTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTime : MonoBehaviour
+{
+
+	public static TileTime instance;
+
+	public static void create() {
+		if (instance == null) {
+			return;
+		}
+
+		GameObject go = Instantiate(new GameObject());
+		go.AddComponent<TileTime>();
+	}
+
+
+    void Awake()
+    {
+		instance = this.GetComponent<TileTime>();
+		create();
+    }
+
+}
