4ddad1b154ec0d05532c41f91ebfc8a3a462c79a
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-08-03 13:47:36 -0700

    sound manager done
    still need a seperate one for ambient/music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource source;
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    /*
+     * PlaySound(sound):
+     * - death
+     * - item
+     *   - drop
+     *   - pickup
+     * - step wall
+     * 
+     * PlaySound(sound, true):
+     * - step
+     * - enemy
+     *   - dopple
+     *   - lurk
+     *   - weep
+     */
+    public void PlaySound(AudioClip sound, bool different=false)
+    {
+        if (different)
+            source.pitch = Random.Range(.95f, 1.05f);
+        else
+            source.pitch = 1f;
+        source.clip = sound;
+        source.Play();
+    }
+}
