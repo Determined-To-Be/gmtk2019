ce15235ce62f15a92a6e92b54180d9b5393af9f5
Patrick <perrinepatrick@gmail.com>
2019-08-03 15:55:28 -0700

    started door prefab

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    bool isOpen = false;
+    bool isLocked = false;
+
+    bool canGoThroughDoor()
+    {
+        return !isLocked;
+    }
+
+    //if open, disable collider
+    void Start()
+    {
+        //TileTime.instance.AddListener();
+    }
+
+}
