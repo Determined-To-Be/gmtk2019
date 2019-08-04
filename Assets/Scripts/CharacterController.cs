1ae10ec1b62ea510a7b8bb354c45d6b25a2ce025
Jacob Golden-Needham <jacob@goldenneedham.com>
2019-08-04 02:57:21 -0700

    Pushable blocks

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,7 +6,10 @@ using UnityEngine.Events;
 public class CharacterController : MonoBehaviour
 {
 	UnityEvent player_event = new UnityEvent();
-	
+
+	public GameObject grid;
+	public SpriteRenderer[] gridRenderer;
+
 	public float moveSpeed = 16f;
 	public bool canMove = true;
 	Vector2 direction;
@@ -29,6 +32,35 @@ public class CharacterController : MonoBehaviour
 		if (canMove && direction.sqrMagnitude > .1f) {
 			TileTime.instance.Tick();
 		}
+	}
+
+	void gridDisplay() {
+		if (grid == null)
+			return;
+
+		if (gridRenderer.Length == 0)
+			return;
+
+		List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
+		//UP -> 0, 1
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, Vector2.up, .8f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, Vector2.up, .8f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+		//Down -> 2, 3
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, Vector2.down, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, Vector2.down, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+		//Left -> 4
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f, Vector2.left, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+		//Right -> 5
+		hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f, Vector2.right, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+		gridRenderer[0].enabled = !(hits[0] || hits[1]);
+		gridRenderer[1].enabled = !(hits[2] || hits[3]);
+		gridRenderer[2].enabled = !hits[4];
+		gridRenderer[3].enabled = !hits[5];
 	}
 
 	void move() {
@@ -57,7 +89,7 @@ public class CharacterController : MonoBehaviour
 
 			Debug.DrawRay(this.transform.position + Vector3.down * .1f, direction * 1.1f);
 		}
-
+		bool canMove = true;
 		foreach (RaycastHit2D hit in hits) {
 			if (hit){ //I hit something I can't move here
 				//Rebound
@@ -67,7 +99,8 @@ public class CharacterController : MonoBehaviour
 
 				ScreenShake.instance.Shake(.1f, .1f);
 				SoundManager.instance.PlaySound(SoundManager.PlayerSound.wall, true);
-				return;
+				canMove = false;
+				break;
 			}
 		}
 
@@ -75,7 +108,23 @@ public class CharacterController : MonoBehaviour
 		//this.transform.position = new Vector3(Mathf.RoundToInt(this.transform.position.x) + .5f, Mathf.RoundToInt(this.transform.position.y) + .5f, 0);
 		//We need to clamp the position to an int and the plus .5 on the x and -.5 on the y
 		SoundManager.instance.PlaySound(SoundManager.PlayerSound.step, true);
-		StartCoroutine(moveTo(this.transform.position + (Vector3)direction.normalized, moveSpeed));
+		if (canMove)
+			StartCoroutine(moveTo(this.transform.position + (Vector3)direction.normalized, moveSpeed));
+		else {
+			StartCoroutine(cantMoveTo(this.transform.position + (Vector3)direction.normalized, moveSpeed));
+		}
+	}
+
+	IEnumerator cantMoveTo(Vector2 goal, float speed) {
+		canMove = false;
+		float val = 1;
+		while (val > .05f){
+			val = Mathf.Lerp(val, 0, speed * Time.deltaTime);
+			yield return null;
+		}
+
+		gridDisplay();
+		canMove = true;
 	}
 
 	IEnumerator moveTo(Vector2 goal, float speed) {
@@ -84,22 +133,26 @@ public class CharacterController : MonoBehaviour
 		
 		while (Vector2.Distance((Vector2)this.transform.position, goal) > .05f) {
 			this.transform.position = Vector3.Lerp(this.transform.position, goal, speed * Time.deltaTime);
+
+			if (item != null)
+				item.transform.position = Vector3.Lerp(item.transform.position, lastpos, speed * Time.deltaTime);
+
 			yield return null;
-		}		
-		this.transform.position = new Vector3(Mathf.RoundToInt(this.transform.position.x), Mathf.RoundToInt(this.transform.position.y), this.transform.position.z);
-
-		if (item != null)
-			item.transform.position = lastpos;
-
+		}
+		//this.transform.position = new Vector3(Mathf.RoundToInt(this.transform.position.x), Mathf.RoundToInt(this.transform.position.y), this.transform.position.z);
+		this.transform.position = goal;
+		grid.transform.position = this.transform.position;
+		gridDisplay();
 		canMove = true;
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
 	{
 		if (coll.transform.tag == "Item") {
-			if (coll.gameObject != null && coll.gameObject == item.gameObject)
-				return;
-			SoundManager.instance.PlaySound(SoundManager.PlayerSound.pickup);
+			if (item != null) {
+				if(item.gameObject != coll.gameObject)
+					SoundManager.instance.PlaySound(SoundManager.PlayerSound.pickup);
+			}
 			item = coll.gameObject.GetComponent<Item>() ;
 		}
 	}
--- Assets/Scripts/PushableBox.cs
+++ Assets/Scripts/PushableBox.cs
@@ -15,8 +15,51 @@ public class PushableBox : PlayerInteractable
 			dir.x = 0;
 
 		//SoundManager.instance.PlaySound(SoundManager.PlayerSound.wall);
-		StartCoroutine(moveTo(this.transform.position + dir, 16f));
+		move(dir);
+		//StartCoroutine(moveTo(this.transform.position + dir, 16f));
 	}
+
+	void move(Vector2 direction)
+	{
+		//We need to prevent the character from moving in the direction of a wall or a collision
+		if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+			direction.y = 0;
+		else
+			direction.x = 0;
+
+		List<RaycastHit2D> hits = new List<RaycastHit2D>();
+		if (direction == Vector2.up) {
+
+			hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, direction, .8f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+			hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, direction, .8f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+			Debug.DrawRay(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, direction * .8f);
+			Debug.DrawRay(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, direction * .8f);
+
+		}
+		else if (direction == Vector2.down){
+
+			hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, direction, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+			hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, direction, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+
+			Debug.DrawRay(this.transform.position + Vector3.down * .1f + Vector3.left * .1f, direction * 1.1f);
+			Debug.DrawRay(this.transform.position + Vector3.down * .1f + Vector3.right * .1f, direction * 1.1f);
+		} else {
+			hits.Add(Physics2D.Raycast(this.transform.position + Vector3.down * .1f, direction, 1.1f, ~LayerMask.GetMask("Player", "Enviroment", "Item")));
+			Debug.DrawRay(this.transform.position + Vector3.down * .1f, direction * 1.1f);
+		}
+
+		foreach (RaycastHit2D hit in hits){
+			if (hit)
+			{
+				//Debug.Log("I hit a wall");
+				return;
+			}
+		}
+
+		StartCoroutine(moveTo(this.transform.position + (Vector3)direction.normalized, 16f));
+	}
+
 
 	IEnumerator moveTo(Vector2 goal, float speed)
 	{
