6fb18ff9e98eaa47a1a2ac7b7d46a1b0dcf85c53
Jacob Golden-Needham <jacob@goldenneedham.com>
2019-08-04 04:49:22 -0700

    Mechanics done!

---
+++ Assets/Scripts/ItemStand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStand : PlayerInteractable
+{
+	Item item = null;
+
+	public override void OnPlayerInteration(){
+		Debug.Log("I am Interacting with player");
+
+		Item tmp = item;
+		Vector3 ttmp = CharacterController.instance.item.transform.position;
+
+		item = CharacterController.instance.item;
+		item.transform.position = this.transform.position;
+
+		CharacterController.instance.item = tmp;
+		CharacterController.instance.item.transform.position = ttmp;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		base.Start();
+    }
+}
---
+++ Assets/Scripts/KeyLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLock : PlayerInteractable
+{
+
+	public Behaviour normalBehavior;
+	public bool isLocked = true;
+	public GameObject lockSprite;
+
+
+	public override void OnPlayerInteration()
+	{
+		if (CharacterController.instance.item.itemType == Item.ItemType.Key){
+			DestroyImmediate(CharacterController.instance.item.gameObject);
+			isLocked = false;
+			lockSprite.SetActive(false);
+			SoundManager.instance.PlaySound(SoundManager.PlayerSound.drop);
+			normalBehavior.enabled = true;
+			DestroyImmediate(this);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		base.Start();
+		normalBehavior.enabled = false;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
