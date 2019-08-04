8c3a41d764406596fef87053c17bf56a46de664f
Jacob Golden-Needham <jacob@goldenneedham.com>
2019-08-04 09:37:39 -0700

    Itemstand

--- Assets/Scripts/ItemStand.cs
+++ Assets/Scripts/ItemStand.cs
@@ -9,13 +9,26 @@ public class ItemStand : PlayerInteractable
 	public override void OnPlayerInteration(){
 
 		Item tmp = item;
-		Vector3 ttmp = CharacterController.instance.item.transform.position;
+		Vector3 ttmp = CharacterController.instance.transform.position;
 
-		item = CharacterController.instance.item;
-		item.transform.position = this.transform.position;
+		if (CharacterController.instance.item != null) {
+			item = CharacterController.instance.item;
+			item.transform.position = this.transform.position;
+			ttmp = CharacterController.instance.item.transform.position;
+		}
+		else {
+			item = null;
+		}
 
-		CharacterController.instance.item = tmp;
-		CharacterController.instance.item.transform.position = ttmp;
+		if (tmp != null)
+		{
+			
+			CharacterController.instance.item = tmp;
+			CharacterController.instance.item.transform.position = ttmp;
+		} else {
+			CharacterController.instance.item = null;
+		}
+		
 	}
 
 	// Start is called before the first frame update
